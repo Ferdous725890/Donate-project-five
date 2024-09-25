c6de701bfce24d071f73b098bdb080d775b1a7ad
ferodus725890 <ferdous725890@gmail.com>
2024-09-25 12:56:36 +0600

    first commit

---
+++ project-five/js/home.js
@@ -0,0 +1,6 @@
+const blog = document.getElementById('blog');
+blog.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log("home clicked");
+    window.location.href="./index.html"
+})
